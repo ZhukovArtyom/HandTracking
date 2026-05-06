e3355d1cefc285f5ef1a83fe64cd8a77fc817229
Артем <buggy_art@mail.ru>
2026-05-06 13:57:49 +0300

    Добавлены действия громкость + и громкость -.
    Прокрутку теперь можно делать зажатием жеста. Громкость также можно менять зажатием

--- backend/gesture_recognizer.py
+++ backend/gesture_recognizer.py
@@ -7,6 +7,8 @@ import win32con
 import keyboard
 import subprocess
 
+from pynput.keyboard import Key, Controller
+
 from config_loader import config
 
 SENSITIVITY_ZONE_PERCENT = config.get('cursor.sensitivity_zone_percent')
@@ -242,23 +244,6 @@ class GestureRecognizer:
 
             del self.active_gestures[gesture_id]
 
-    def perform_left_click(self):
-        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
-
-    def perform_left_click_release(self):
-        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0, 0, 0)
-
-    def perform_right_click(self):
-        win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTDOWN, 0, 0, 0, 0)
-
-    def perform_right_click_release(self):
-        win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTUP, 0, 0, 0, 0)
-    def perform_wheel_up(self):
-        for _ in range(1):
-            win32api.mouse_event(win32con.MOUSEEVENTF_WHEEL, 0, 0, 120, 0)
-    def perform_wheel_down(self):
-        for _ in range(1):
-            win32api.mouse_event(win32con.MOUSEEVENTF_WHEEL, 0, 0, -120, 0)
 
     def _perform_action(self, gesture, on_release: bool = False):
         gesture_type = gesture['type']
@@ -266,22 +251,40 @@ class GestureRecognizer:
         if gesture_type == "system":
             action = gesture['on_press'] if not on_release else gesture['on_release']
             if action == 'left_click_press':
-                self.perform_left_click()
+                win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
 
             elif action == 'left_click_release':
-                self.perform_left_click_release()
+                win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0, 0, 0)
 
             elif action == 'right_click_press':
-                self.perform_right_click()
+                win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTDOWN, 0, 0, 0, 0)
 
             elif action == 'right_click_release':
-                self.perform_right_click_release()
+                win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTUP, 0, 0, 0, 0)
 
             elif action == 'wheel_up':
-                self.perform_wheel_up()
+
+                win32api.mouse_event(win32con.MOUSEEVENTF_WHEEL, 0, 0, 30, 0)
+                time.sleep(0.03)
+                self.on_gesture_release(gesture['id'])
 
             elif action == 'wheel_down':
-                self.perform_wheel_down()
+
+                win32api.mouse_event(win32con.MOUSEEVENTF_WHEEL, 0, 0, -30, 0)
+                time.sleep(0.03)
+                self.on_gesture_release(gesture['id'])
+
+            elif action == 'volume_up':
+                keyboard.press('volume up')
+                time.sleep(0.03)
+                self.on_gesture_release(gesture['id'])
+
+
+            elif action == 'volume_down':
+                keyboard.press('volume down')
+                time.sleep(0.03)
+                self.on_gesture_release(gesture['id'])
+
             else:
                 print(f"Неизвестное действие: {action}")
 
